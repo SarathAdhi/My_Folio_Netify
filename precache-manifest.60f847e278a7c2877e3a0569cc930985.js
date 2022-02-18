f133cd511c22f021933b23e29dd3f2d6903683c5
SarathAdhi <s.sarathadhithya@gmail.com>
2022-02-18 20:32:43 +0530

    Update-Animations

---
+++ precache-manifest.60f847e278a7c2877e3a0569cc930985.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "408792a3aa4418e564a2f05a346fb952",
+    "url": "/index.html"
+  },
+  {
+    "revision": "22e20d10020bb461b73e",
+    "url": "/static/css/main.6d379373.chunk.css"
+  },
+  {
+    "revision": "02b6423933fa945f652d",
+    "url": "/static/js/2.d95868f8.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/static/js/2.d95868f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22e20d10020bb461b73e",
+    "url": "/static/js/main.98a6a5e2.chunk.js"
+  },
+  {
+    "revision": "ae7bb63ef3bb4426842b",
+    "url": "/static/js/runtime-main.6f3f843c.js"
+  },
+  {
+    "revision": "7153f3b92a8f3382079886e037ad494b",
+    "url": "/static/media/404error.7153f3b9.png"
+  },
+  {
+    "revision": "069cf9afcc64b6ad201b3c67c02c2494",
+    "url": "/static/media/ReactJS for Beginners.069cf9af.png"
+  },
+  {
+    "revision": "1256a4a5ffe9512d642322e0f32cf9b5",
+    "url": "/static/media/arduino.1256a4a5.jpeg"
+  },
+  {
+    "revision": "683820c52205b0b744bce3f802eca385",
+    "url": "/static/media/hdoc.683820c5.png"
+  },
+  {
+    "revision": "b604dfc01baa501e0f8b5c9f9c1133b3",
+    "url": "/static/media/hub.b604dfc0.png"
+  },
+  {
+    "revision": "f3c7753a16f528ceab2737c8fe7776f8",
+    "url": "/static/media/image.f3c7753a.png"
+  },
+  {
+    "revision": "647d4024c2f5ec08beab635552728dd1",
+    "url": "/static/media/js.647d4024.png"
+  },
+  {
+    "revision": "78f0fb179f708bce2c33ab7ee0679488",
+    "url": "/static/media/quiz.78f0fb17.png"
+  },
+  {
+    "revision": "c8b49a562ceda21d8c615ad8ef1d7359",
+    "url": "/static/media/resume.c8b49a56.pdf"
+  },
+  {
+    "revision": "ddfbed2af3e8475f8462d528fd003e10",
+    "url": "/static/media/todo.ddfbed2a.png"
+  }
+]);
